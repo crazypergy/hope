77f229614cccc05756a87e05c75e4e3ba78da970
gazayas <g-zayas@hotmail.com>
2017-08-24 11:53:00 +0900

    Specify model 関連

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,10 @@
 class Photo < ApplicationRecord
+  belongs_to :album
+
+  has_attached_file :image,
+    path: ":rails_root/public/images/:id/:filename",
+    url: "/images/:id/:filename"
+
+  # Need to write validations for this model, but this is pretty cool
+  do_not_validate_attachment_file_type :image
 end
