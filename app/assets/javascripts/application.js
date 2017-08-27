555f6e5ee58e9a3bd8c14908fba3970668992d4f
gazayas <g-zayas@hotmail.com>
2017-08-28 08:58:27 +0900

    Put individual photo inputs in their own divs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,16 +31,20 @@
 
 function add_photo_input() {
   var photos_div = document.getElementById('photos');
-  $(photos_div).prepend(
-    // TODO: I don't like how type="file" makes the file name show up on the side of the button.
-    // It's a dirty hack that I don't REALLY want to do, but it's possible to hide the input,
-    // and make an entirely new button that will trigger #display_picture_from($(this).prev());.
-    // Again, this could potentially make a mess with the #prev and #next stuff I was talking about
-    // earlier, so try to think of a good, clean solution.
-    '<input class="photo" name="images[]" type="file" onchange="display_picture_from(this);" />' +
-    '<img src="#" alt=""/>' +
-    '<button type="button" onclick="delete_photo(this);" style="display: none;">Delete photo</button>'
-  );
+
+  // TODO: I don't like how type="file" makes the file name show up on the side of the button.
+  // It's a dirty hack that I don't REALLY want to do, but it's possible to hide the input,
+  // and make an entirely new button that will trigger #display_picture_from($(this).prev());.
+  // Again, this could potentially make a mess with the #prev and #next stuff I was talking about
+  // earlier, so try to think of a good, clean solution.
+  var individual_photo_div =
+    '<div class="photo_wrapper">' +
+      '<input class="photo_input" name="images[]" type="file" onchange="display_picture_from(this);" />' +
+      '<img src="#" alt=""/>' +
+      '<button type="button" onclick="delete_photo(this);" style="display: none;">Delete photo</button>' +
+    '</div>';
+
+  $(photos_div).prepend(individual_photo_div);
 }
 
 function display_picture_from(input) {
