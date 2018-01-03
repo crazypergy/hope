ae6f0dcd7af566f87be8e7391208663a8545e416
gazayas <g-zayas@hotmail.com>
2018-01-03 12:13:54 -0500

    Add default url options (devise)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Add default url options for mailer as the initial devise prompt suggests
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
