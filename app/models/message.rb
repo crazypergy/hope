b09b81583317ba71d6bff130ad3b3780fcfe4955
gazayas <g-zayas@hotmail.com>
2018-01-13 13:25:36 +0900

    Delete deleted title attribute from views

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,9 @@
 class Message < ApplicationRecord
+
   # No need for datetime validation since select is being used
-  validates :title,
-    presence: true, length: { maximum: 50 }
 
   validates :link,
     presence: true,
     length: { maximum: 100 },
     format: { with: /www\.youtube\.com\/watch\?v=|https:\/\/youtu\.be\// }
-    # iframe format
-    # format: { with: /(\A<iframe\swidth)(.*)(src=\"https:\/\/www\.youtube\.com\/embed)(.*)(><\/iframe>\Z)/ }
 end
